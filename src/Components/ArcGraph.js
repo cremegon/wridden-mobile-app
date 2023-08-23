a8837b1f2d720bc8472f634a8533fea26dcdaaf3
Hussain Ahmed Shaikh <hussainahmed.shaikh@student.griffith.ie>
2023-08-23 14:12:50 +0100

    TOPSHEET WORKS
    time for boogie

--- src/Components/ArcGraph.js
+++ src/Components/ArcGraph.js
@@ -1,12 +1,73 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect } from "react";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import Animated, {
+  event,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const ArcGraph = () => {
+  const translateY = useSharedValue(0);
+  const context = useSharedValue({ y: 0 });
+
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = { y: translateY.value };
+    })
+    .onUpdate((event) => {
+      console.log(translateY.value);
+      translateY.value = event.translationY + context.value.y;
+      translateY.value = Math.min(translateY.value, SCREEN_HEIGHT / 3);
+    })
+    .onEnd(() => {
+      if (translateY.value > SCREEN_HEIGHT / 6) {
+        translateY.value = withSpring(SCREEN_HEIGHT / 3);
+      } else if (translateY.value < SCREEN_HEIGHT / 4) {
+        translateY.value = withSpring(SCREEN_HEIGHT / 10);
+      }
+    });
+
+  useEffect(() => {
+    translateY.value = withSpring(SCREEN_HEIGHT / 10);
+  }, []);
+
+  const rTopSheetStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  });
   return (
-    <View>
-      <Text>ArcGraph</Text>
-    </View>
+    <GestureDetector gesture={gesture}>
+      <Animated.View style={[styles.topSheet, rTopSheetStyle]}>
+        <View style={styles.line}></View>
+      </Animated.View>
+    </GestureDetector>
   );
 };
+
+const styles = StyleSheet.create({
+  topSheet: {
+    backgroundColor: "royalblue",
+    height: SCREEN_HEIGHT,
+    width: "100%",
+    position: "absolute",
+    bottom: SCREEN_HEIGHT,
+    borderRadius: 25,
+    zIndex: 1,
+  },
+  line: {
+    backgroundColor: "black",
+    height: 4,
+    borderRadius: 30,
+    width: 75,
+    alignSelf: "center",
+    marginVertical: SCREEN_HEIGHT / 1.03,
+  },
+});
 
 export default ArcGraph;
--- src/Screens/TestTop.js
+++ src/Screens/TestTop.js
@@ -1,15 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ArcGraph from "../Components/ArcGraph";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const TestTop = () => {
   return (
-    <SafeAreaView>
-      <ArcGraph />
-      <Text>TestTop</Text>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <ArcGraph />
+        <Text>TestTop</Text>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default TestTop;
