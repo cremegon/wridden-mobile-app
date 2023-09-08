b7ba3c8367052e9e8d93cd1add26c8f3ced2e273
cremegon <hussainahmed.shaikh@student.griffith.ie>
2023-09-08 07:30:23 +0500

    another fix

--- src/Screens/StoryHome/StoryList.js
+++ src/Screens/StoryHome/StoryList.js
@@ -27,14 +27,9 @@ const StoryList = ({ navigation }) => {
     );
   }, []);
 
-  const titleList = [
-    { title: "The Big Bad Wolf", nav: "Chapter Select" },
-    { title: "Beanstalkers", nav: "Story 2" },
-    { title: "Snowstorm and the Seven Towers", nav: "Story 3" },
-    { title: "Glorious Gentlemen", nav: "Story 4" },
-  ];
+  const titleList = ["Story 1", "Story 2", "Story 3", "Story 4"];
 
-  return (
+  nav: return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "rgba(226,237,236,0.4)" }}>
       <AppHeader
         title={"Story List"}
@@ -45,16 +40,20 @@ const StoryList = ({ navigation }) => {
       <ScrollView>
         <View style={{ marginBottom: 100, marginTop: 20 }}>
           {(stories &&
-            stories.map((item, index) => (
-              <Card
-                key={item.id}
-                title={item.title}
-                navigation={navigation}
-                image={btnData[2].source}
-                link={item.nav}
-                params={item.title}
-              />
-            ))) || (
+            stories.map((item, index) => {
+              random_nav =
+                titleList[Math.floor(Math.random() * titleList.length)];
+              return (
+                <Card
+                  key={item.id}
+                  title={item.title}
+                  navigation={navigation}
+                  image={btnData[2].source}
+                  link={random_nav}
+                  params={item.title}
+                />
+              );
+            })) || (
             <Text style={{ color: "black", fontSize: 25, textAlign: "center" }}>
               You do not have any stories.
             </Text>
